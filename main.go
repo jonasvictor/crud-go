2ca3440b43d26a269a00e3dae3ce67e04bab0bce
Jonas Victor <jonas.victor@dcx.ufpb.br>
2023-03-12 23:16:34 -0300

    commitando os primeiros arquivos

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	fmt.Println(os.Getenv("TEST"))
+}
