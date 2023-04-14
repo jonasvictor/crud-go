713902bb76cf9208e472423873ce14dea96cb6b6
Jonas Victor <jonas.victor@dcx.ufpb.br>
2023-04-14 00:22:53 -0300

    rotas da aplicação

---
+++ src/controller/findUser.go
@@ -0,0 +1,9 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func FindUserByID(c *gin.Context) {
+}
+
+func FindUserByEmail(c *gin.Context) {
+}
---
+++ src/controller/routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jonasvictor/crud-go/src/controller"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+
+	r.GET("/getUserById/:userId", controller.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
+}
