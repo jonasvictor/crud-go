b06094c2d1632f7f32f311a1dfa2a43499247f3a
Jonas Victor <jonas.victor@dcx.ufpb.br>
2023-04-17 00:01:47 -0300

    Adicionando logs e alterações em rest_err e createUser

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/jonasvictor/crud-go/src/configuration/logger"
+	"github.com/jonasvictor/crud-go/src/controller/routes"
 )
 
 func main() {
+	logger.Info("About to start the application")
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
-	fmt.Println(os.Getenv("TEST"))
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+
 }
